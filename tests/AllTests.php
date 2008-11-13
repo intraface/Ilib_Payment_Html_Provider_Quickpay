b9b70092e156fb56c65638dc4b75c2ecb57694b8
Sune T. Jensen <sj@sunet.dk>
2008-11-13 21:40:19 +0000

    Tried to make the it remove template from whitelist - no success.
    git-svn-id: http://svn.intraface.dk/intrafacepublic/3Party/Ilib_Payment_Html_Provider_Quickpay@1326 2fc64ab5-e821-0410-98e1-ee8f871d7b73

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -5,6 +5,7 @@ require_once 'PHPUnit/TextUI/TestRunner.php';
 require_once 'Ilib/ClassLoader.php';
 
 PHPUnit_Util_Filter::addDirectoryToWhitelist(realpath(dirname(__FILE__) . '/../src/'));
+PHPUnit_Util_Filter::removeFileFromWhitelist(realpath(dirname(__FILE__) . '/../src/Ilib/Payment/Html/Provider/Quickpay/templates/').'/payment-input-tpl.php');
 
 class AllTests
 {
